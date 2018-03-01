c3e8326e157cc39cdce44a068c2f6b76a6bce7f2
Norrismi <Norrismi0@gmail.com>
2018-02-28 23:18:23 -0600

    first commit

---
+++ src/components/StorePicker.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+class StorePicker extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>I am the store picker!!!</p>
+
+            </div>
+        )
+    }
+}
+
+export default StorePicker;
